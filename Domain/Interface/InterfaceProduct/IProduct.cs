7e3f9f9d960c34a76d5169ad0be6b1ea0c626e67
Alessandro Tulioli <alessandro.smarapd@hotmail.com>
2021-07-19 21:14:44 -0300

    Cria Interfaces e classe de serviço

---
+++ Domain/Interface/InterfaceProduct/IProduct.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Generics;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Interface.InterfaceProduct
+{
+    public interface IProduct : IGeneric<Product>
+    {
+
+    }
+}
---
+++ Domain/Services/ServiceProduct.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Interface.InterfaceProduct;
+using Domain.Interface.InterfaceServices;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public class ServiceProduct : IServiceProduct
+    {
+        private readonly IProduct _iProduct;
+
+        public ServiceProduct(IProduct iProduct)
+        {
+            _iProduct = iProduct;
+        }
+
+        public async Task AddProduct(Product product)
+        {
+            var validaNome = product.ValidarPropriedadesString(product.Nome, "Nome");
+
+            var validaValor = product.ValorPropriedadeDecimal(product.Valor, "Valor");
+
+            if (validaNome && validaValor)
+            {
+                product.Estado = true;
+                await _iProduct.Add(product);
+            }
+        }
+
+        public async Task UpdateProduct(Product product)
+        {
+            var validaNome = product.ValidarPropriedadesString(product.Nome, "Nome");
+
+            var validaValor = product.ValorPropriedadeDecimal(product.Valor, "Valor");
+
+            if(validaNome && validaValor)
+            {
+                await _iProduct.Update(product);
+            }
+        }
+    }
+}
