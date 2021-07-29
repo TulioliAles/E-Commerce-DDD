f59087014e30c9cf311c3f2d3bcbb57a85704677
Alessandro Tulioli <alessandro.smarapd@hotmail.com>
2021-07-28 21:44:50 -0300

    Cria Classe de Contexto no Infrastructure

---
+++ Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Configuration
+{
+    public class ContextBase : DbContext
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Produto { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConnectionConfig());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        private string GetStringConnectionConfig()
+        {
+            string strCon = "Data Source=(localdb)/ServidorLocal;Initial Catalog=DDD_ECOMMERCE;Integrated Security=false;User ID=****;Password=****;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+            return strCon;
+        }
+    }
+}
